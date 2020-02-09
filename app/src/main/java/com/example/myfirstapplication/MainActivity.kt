435b3098cc56f7cfb8db1c809689a70ddec84673
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-02-09 22:14:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myfirstapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Thread.sleep
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener { progressUp() }
+    }
+
+    private fun progressUp(){
+        var x = 1
+        while(x < 100){
+            x =+ 10
+            progressBar.progress = x
+            sleep(1000)
+        }
+
+    }
+
+}
