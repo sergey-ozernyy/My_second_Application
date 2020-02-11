a59b87a28eae124f16d978c4302d9f66da0ebfd9
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-02-11 20:08:13 +0300

    По нажатию кнопки "Start" прибавляется 10% progress,элементы расположены на макете нормально

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -10,14 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener { progressBar.progress += 10 }
-    }
+        button1.setOnClickListener { progressBar1.progress += 10
+            if (progressBar1.progress == 100) {progressBar1.progress = 10}}
+        button2.setOnClickListener { progressBar2.progress += 10 }
+        button3.setOnClickListener { progressBar3.progress += 10 }
 
+    }
+    //Написать класс и нормально проинициализировать объекты
     public fun progressUp(){
         var x = 1
         while(x < 100){
             x =+ 10
-            progressBar.progress = x
+            progressBar1.progress = x
             sleep(1000)
         }
 
